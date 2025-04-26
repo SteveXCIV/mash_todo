a56b8d5a034722c4ca66cf8aa5baddd012e99371
Steve Troetti <SteveXCIV@gmail.com>
2025-04-26 15:31:13 -0400

    get htmx working

--- src/routes.rs
+++ src/routes.rs
@@ -1,6 +1,13 @@
 use crate::views;
-use axum::{Router, routing::get};
+use axum::{
+    Router,
+    routing::{get, post},
+};
+use tower_http::services::ServeDir;
 
 pub fn create_router() -> Router {
-    Router::new().route("/", get(views::home))
+    Router::new()
+        .nest_service("/public", ServeDir::new("public"))
+        .route("/", get(views::home))
+        .route("/api/v1/thebutton", post(views::clicked_the_button))
 }
--- src/views.rs
+++ src/views.rs
@@ -5,9 +5,20 @@ pub async fn home() -> Markup {
         (DOCTYPE)
         head {
             title { "Home" }
+            script src="/public/js/htmx_2.0.4/htmx.min.js" type="text/javascript" {}
         }
         body {
             p { "Hello world!" }
+            br;
+            button hx-post="/api/v1/thebutton" hx-trigger="click" hx-swap="outerHTML" {
+                "Click me!"
+            }
         }
+    }
+}
+
+pub async fn clicked_the_button() -> Markup {
+    html! {
+        p { "You clicked the button!" }
     }
 }
