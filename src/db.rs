0dd474ce567751e0d452e5136ca69f1d31f67ec4
Steve Troetti <SteveXCIV@gmail.com>
2025-04-27 01:59:12 -0400

    add db test

--- src/db.rs
+++ src/db.rs
@@ -16,3 +16,24 @@ pub async fn create_pool(connection_string: &str) -> Result<SqlitePool, Error> {
     MIGRATOR.run(&pool).await?;
     Ok(pool)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use sqlx::query_scalar;
+
+    #[tokio::test]
+    async fn test_create_pool() {
+        let pool = create_pool("sqlite::memory:")
+            .await
+            .expect("failed to create pool");
+
+        let actual = query_scalar::<_, u8>(
+            "SELECT COUNT(*) FROM sqlite_master WHERE type='table' AND name='todos'",
+        )
+        .fetch_one(&pool)
+        .await.expect("failed to run query");
+
+        assert_eq!(actual, 1, "todos table does not exist");
+    }
+}
